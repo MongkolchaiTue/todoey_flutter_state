f0a347253d584d110f27bcc8dddc1e8d5a621434
MTue2023 <{ID}+{username}@users.noreply.github.com>
2024-03-05 00:47:23 +0700

    add checkboxState, global state

--- lib/screens/task_title.dart
+++ lib/screens/task_title.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
-class TasksTitle extends StatelessWidget {
+class TasksTitle extends StatefulWidget {
+
   const TasksTitle({
     super.key,
   });
 
   @override
+  State<TasksTitle> createState() => _TasksTitleState();
+}
+
+class _TasksTitleState extends State<TasksTitle> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is a Task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {
-          print(value);
-        },
+      title: Text(
+        'This is a Task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing:  TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: () => (checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+            print("check box state2");
+          },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({super.key, required this.checkboxState, required this.toggleCheckboxState});
+
+  bool checkboxState;
+  Function() toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (newValue) => toggleCheckboxState,
     );
   }
 }
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -10,21 +10,32 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.lightBlueAccent,
-        child: const Icon(
-          Icons.add,
-        ),
-        onPressed: (){
-          showModalBottomSheet(
-              context: context,
-              builder: (context) => AddTaskScreen(),
-          );
-        },
+          backgroundColor: Colors.lightBlueAccent,
+          child: const Icon(
+            Icons.add,
+          ),
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => SingleChildScrollView(
+                        child: Container(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: const AddTaskScreen(),
+                    )));
+          }
+          // onPressed: (){
+          //   showModalBottomSheet(
+          //       context: context,
+          //       builder: (context) => const AddTaskScreen(),
+          //   );
+          // },
 
-      ),
+          ),
       body: Container(
-        padding: const EdgeInsets.only(top: 60, left:  0, right: 0,bottom: 30),
-        child: Column (
+        padding: const EdgeInsets.only(top: 60, left: 0, right: 0, bottom: 30),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -43,14 +54,12 @@ class TasksScreen extends StatelessWidget {
             ),
             const Padding(
               padding: EdgeInsets.only(left: 20.0, top: 10.0),
-              child: Text(
-                'Todoey',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                )
-              ),
+              child: Text('Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  )),
             ),
             const Padding(
               padding: EdgeInsets.only(left: 20.0),
@@ -69,12 +78,11 @@ class TasksScreen extends StatelessWidget {
               child: Container(
                 height: 300.0,
                 decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
-                  )
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )),
                 child: const TasksList(),
               ),
             ),
@@ -84,7 +92,3 @@ class TasksScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-
